6a921ac32382c7a9986680defe237f1832e0d563
SpeedVan <61458673@qq.com>
2019-09-26 14:48:51 +0800

    单测

--- type/maybe/MaybeInt_test.go
+++ type/maybe/MaybeInt_test.go
@@ -1,6 +1,7 @@
 package maybe
 
 import (
+	"errors"
 	"fmt"
 	"reflect"
 	"testing"
@@ -25,4 +26,25 @@ func Test(t *testing.T) {
 	if v, ok := val2.(Integer); ok {
 		fmt.Println(v * 3)
 	}
+
+	switch t := reflect.TypeOf(errors.New("123")); t {
+	case subtypeof(t, reflect.TypeOf(*NullError)):
+		println("subtype:nullError")
+	case subtypeof(t, reflect.TypeOf(Integer(1))):
+		println("subtype:Integer")
+	default:
+		println("fail")
+	}
+}
+
+func subtypeof(s reflect.Type, t reflect.Type) reflect.Type {
+	println("s:" + s.Name() + ",t:" + t.Name())
+	if s.ConvertibleTo(t) {
+		return s
+	}
+	return t
+}
+
+func instanceof(v interface{}, t reflect.Type) bool {
+	return false
 }
