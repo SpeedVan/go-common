0679d2b623a581eaa6870599708fcf88dbc53866
finup <finup@finupdeMacBook-Pro.local>
2019-11-20 14:55:02 +0800

    k8s clientset & some converter

---
+++ convert/BaseConverter.go
@@ -0,0 +1,16 @@
+package convert
+
+// PtrOfInt pointer of int type value
+func PtrOfInt(v int) *int {
+	return &v
+}
+
+// PtrOfInt32 pointer of int32 type value
+func PtrOfInt32(v int32) *int32 {
+	return &v
+}
+
+// PtrOfInt64 pointer of int64 type value
+func PtrOfInt64(v int64) *int64 {
+	return &v
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/SpeedVan/go-common
+
+go 1.13
+
+require (
+	github.com/gorilla/mux v1.7.3
+	k8s.io/apiextensions-apiserver v0.0.0-20191117020858-b615a37f53e7
+	k8s.io/client-go v11.0.0+incompatible
+)
